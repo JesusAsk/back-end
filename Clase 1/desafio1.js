afcfe8634912959278c0e4b3f24008963a532d80
Jesus Ascanio <jesus.ascanio.1728@gmail.com>
2023-03-26 20:53:53 -0300

    compretando el desafio 1

--- Clase 1/desafio1.js
+++ Clase 1/desafio1.js
@@ -5,46 +5,59 @@ class Productmanager{
 
     }
 
-    getproducts(){
+    getProducts(){
         return this.products
     }
 
-    addProduct(title,description,price, stock,id){
+    addProduct(title,description,price, thumbnail, stock, code , ){
 
-        let id_producto = (this.getProducts()).length;
+        let id_producto = this.getProducts().length;
 
         let producto = {
             title:title,
-            price:price,
             description:description,
-            code:code,
-            id:id,
+            price:price,
+            thumbnail:thumbnail,
             stock:stock,
-            thumbnail:thumbnail
-
-
+            code:code,
+            id: ++id_producto
+            
         }
 
-        this.products.push(producto)
-        return this.products;
-    }
-
-    getProductsById(id_producto){
-        let producto = this.products.find(producto => producto.id == id_producto)
-        if (producto){
-            return producto;
-        }else{
-            return console.log("not found")
+        if (!title|| !description || !price || !code || !stock) {
+            return console.log ( `Todos los campos son obligatorios.`)
         }
-    }
 
+        let codigo = this.products.find((prod) => prod.code == producto.code)
+            if (codigo){
+                return "El codigo ya existe, ingrese uno nuevo"
+            }else{
+                this.products.push(producto);
+                return this.products;
+            }
+
+
+    }
+        
+        getProductsById(id_producto){
+
+            let producto = this.products.find(producto => producto.id === id_producto)
+
+            if(producto){
+                return producto;
+            }else {
+                return console.log("not found");
+            }
+
+        }
+    
 }
 
 
 const Productos = new Productmanager()
-Productos.addProduct( "Lomo", "ternera", 3490 , 3 );
-Productos.addProduct( "colita", "ternera", 3140 , 6);
+Productos.addProduct( "Lomo", "Carne de ternera de primera calidad ", 3490 , "#" , 4 , 20 , 1 );
+Productos.addProduct( "Colita de Cuadril", "Carne de ternera de primera calidad" , 3140 , "#", 6 , 30 ,2 );
 
-console.log(Productos.getProductsById(1))
+console.log(Productos.getProductsById(2))
 
 
